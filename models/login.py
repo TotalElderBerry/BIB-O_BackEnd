69f8d18de8f1b28b4ac9156aeea29e6608fbfa20
ampats04 <jeremyandyampatin@gmail.com>
2024-03-23 18:01:15 +0800

    feature(api): added login api

---
+++ models/login.py
@@ -0,0 +1,27 @@
+from database import engine
+from sqlalchemy import text
+from flask import session
+
+
+def login_event_organizer(data):
+
+    with engine.connect() as conn:
+
+        query = text("SELECT * FROM event_organizer WHERE email = :email")
+        dict_text = dict(email = data["email"])
+
+        result = conn.execute(query,dict_text)
+
+        rows = result.fetchone()
+
+        if rows[4] == data["email"] and rows [5] == data["password"]:
+            session["username"] = data["email"]
+            return True
+        else:
+            return False
+    
+        
+
+
+
+    
