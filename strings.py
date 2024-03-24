5e2dcf0241f716cf5d9cd6eb3a5964cf585b0528
ampats04 <jeremyandyampatin@gmail.com>
2024-03-24 13:57:20 +0800

    refactor(app): organized folders

--- strings.py
+++ strings.py
@@ -1,10 +1,8 @@
+EMAIL_PASSWORD_EMPTY = "Email or Password cannot be empty!"
+INVALID_PASSWORD = "Password is Incorrect!"
+EMAIL_DOES_NOT_EXIST = "Email does not Exist!"
+USERNAME_EXISTS = "Username already exists!"
+LOGIN_SUCESS = "Login Sucess!"
 
-class CustomError(Exception):
-    def __init__(self, message, status_code):
-        super().__init__(message)
-        self.status_code = status_code
 
-EMAIL_PASSWORD_EMPTY = CustomError("Email or Password cannot be empty!", 400)
-INVALID_CREDENTIALS = CustomError("Invalid Email or Password!", 400)
-USERNAME_EXISTS = CustomError("Username already exists!", 400)
-LOGIN_SUCESS = CustomError("Login Sucess!", 200)
+
