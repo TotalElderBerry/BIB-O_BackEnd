39d52fd05a0513b500c43e2f9bdfb74bbaae72a5
ampats04 <jeremyandyampatin@gmail.com>
2024-03-30 00:08:10 +0800

    chore(app): converted strings to JSON format

--- strings.py
+++ strings.py
@@ -1,30 +1,35 @@
-EMAIL_PASSWORD_EMPTY = "Email and Password cannot be empty!"
-INVALID_PASSWORD = "Password is Incorrect!"
-INVALID_EMAIL = "Email is Incorrect!"
+EMAIL_PASSWORD_EMPTY = {"message": "Email and Password cannot be empty!"}
+INVALID_PASSWORD = {"message": "Password is Incorrect!"}
+INVALID_EMAIL = {"message": "Email is Incorrect!"}
 
-LOGIN_SUCESS = "Login Sucess!"
-LOGOUT_SUCESS = "Logout Sucessfully!"
-REGISTRATION_SUCESS = "Registered Sucessfully!"
+LOGIN_SUCESS = {"message": "Login Sucess!"}
+LOGOUT_SUCESS = {"message": "Logout Sucessfully!"}
+REGISTRATION_SUCESS = {"message": "Registered Sucessfully!"}
 
 
 # Event Organizer data
 
-NAME_EMPTY = "Name is required!"
-ADDRESS_EMPTY = "Address is required!"
-EMAIL_EMPTY = "Email is required!"
-PASSWORD_EMPTY = "Password is required!"
-STATUS_EMPTY = "Status is required!"
-EMAIL_EXISTS = "Username already exists!"
+NAME_EMPTY = {"message": "Name is required!"}
+ADDRESS_EMPTY = {"message": "Address is required!"}
+EMAIL_EMPTY = {"message": "Email is required!"}
+PASSWORD_EMPTY = {"message": "Password is required!"}
+STATUS_EMPTY = {"message": "Status is required!"}
+EMAIL_EXISTS = {"message": "Username already exists!"}
 
 # Events
-
-NO_EVENTS = "No Events at the moment"
-EVENT_DOESNT_EXIST = "Event does not exist."
-EVENT_NAME_EMPTY = "Event name is required!"
-EVENT_DATE_EMPTY = "Event date is required!"
-EVENT_EXISTS = "Event already existed!"
-EVENT_SUCESS = "Event created sucessfully!"
+NO_EVENTS = {"message": "No Events at the moment"}
+EVENT_DOESNT_EXIST = {"message": "Event does not exist."}
+EVENT_NAME_EMPTY = {"message": "Event name is required!"}
+EVENT_DATE_EMPTY = {"message": "Event date is required!"}
+EVENT_EXISTS = {"message": "Event already existed!"}
+EVENT_SUCESS = {"message": "Event created sucessfully!"}
 
 # Photographer
-PHOTOGRAPHER_EXISTS = "Photographer already registered!"
-PHOTOGRAPHER_REGISTERED_SUCCESSFULLY = "Photographer reigstered sucessfully!"
+PHOTOGRAPHER_EXISTS = {"message": "Photographer already registered!"}
+PHOTOGRAPHER_REGISTERED_SUCCESSFULLY = {
+    "message": "Photographer reigstered sucessfully!"
+}
+
+# Runner
+FIRST_NAME_EMPTY = {"message": "First name is required!"}
+LAST_NAME_EMPTY = {"message": "Last name is required!"}
