7060ca13a462aab8c51b98151c6cd83f642d70f3
ampats04 <jeremyandyampatin@gmail.com>
2024-03-30 22:35:08 +0800

    chore(runers): converted return statements to JSON format

--- photographer/photographer.py
+++ photographer/photographer.py
@@ -1,6 +1,6 @@
-from database import engine
+from database import engine,
 from sqlalchemy import text, exc
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from strings import *
 
 
@@ -22,42 +22,31 @@ photographer = Blueprint("photographers", __name__)
 def register_photographer(event_organizer_id):
 
     data = request.form
-    error = None
-
+   
     if request.method == "POST":
         if data["name"] is None:
-            error = NAME_EMPTY
+            return jsonify(NAME_EMPTY)
         elif data["address"] is None:
-            error = ADDRESS_EMPTY
+            return jsonify(ADDRESS_EMPTY)
         elif data["email"] is None:
-            error = EMAIL_EMPTY
+            return jsonify(EMAIL_EMPTY)
         elif data["status"] is None:
-            error = STATUS_EMPTY
+            return jsonify(STATUS_EMPTY)
 
-    if error is None:
-
-        try:
-            with engine.connect() as conn:
-                query = text(
-                    "INSERT INTO photographer(event_organizer_id, name, address, email, status) VALUES(:event_organizer_id, :name, :address, :email, :status)"
-                )
-                params = dict(
-                    event_organizer_id=event_organizer_id,
-                    name=data["name"],
-                    address=data["address"],
-                    email=data["email"],
-                    status=data["status"],
-                )
-
-                conn.execute(query, params)
-
-                conn.commit()
-        except exc.IntegrityError as e:
-            error = PHOTOGRAPHER_EXISTS
-
-        else:
-            redirect(
-                url_for("photographer"), message=PHOTOGRAPHER_REGISTERED_SUCCESSFULLY
+        with engine.connect() as conn:
+            query = text(
+                "INSERT INTO photographer(event_organizer_id, name, address, email, status) VALUES(:event_organizer_id, :name, :address, :email, :status)"
             )
 
-    return render_template("photographer.vue")
+            params = dict(
+                event_organizer_id=event_organizer_id,
+                name=data["name"],
+                address=data["address"],
+                email=data["email"],
+                status=data["status"],
+            )
+
+            conn.execute(query, params)
+
+            conn.commit()
+        return jsonify(PHOTOGRAPHER_REGISTERED_SUCCESSFULLY)
