062ea66c8ffb795567bdfab1a32f685f64a2467f
ampats04 <jeremyandyampatin@gmail.com>
2024-04-07 19:49:49 +0800

    security(cors): added cors in main app file

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, render_template
 from event_organizer.event_organizer import event_organizer
 from event.events import events
 from photographer.photographer import photographer
 from runner.runners import runners
 from auth.auth import auth
 import uuid
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = uuid.uuid4().hex
 
+CORS(app)
 # blue_prints
 app.register_blueprint(event_organizer, url_prefix="/event_organizer")
 app.register_blueprint(events, url_prefix="/event")
@@ -21,7 +23,7 @@ app.register_blueprint(auth, url_prefix="/admin")
 # Routes
 @app.route("/")
 def index():
-    return render_template("index.js")
+    return "Hello world"
 
 
 if __name__ == "__main__":
