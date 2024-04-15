b22204c887a334e3edab03b26e9e4807a5e2fa64
ampats04 <jeremyandyampatin@gmail.com>
2024-04-15 22:40:54 +0800

    refactor(strings): added succes message

--- strings.py
+++ strings.py
@@ -1,40 +1,52 @@
-EMAIL_PASSWORD_EMPTY = {"message": "Email and Password cannot be empty!"}
-INVALID_PASSWORD = {"message": "Password is Incorrect!"}
-INVALID_EMAIL = {"message": "Email is Incorrect!"}
+EMAIL_PASSWORD_EMPTY = {
+    "success": False,
+    "message": "Email and Password cannot be empty!",
+}
+INVALID_PASSWORD = {"success": False, "message": "Password is Incorrect!"}
+INVALID_EMAIL = {"success": False, "message": "Email is Incorrect!"}
 
-LOGIN_SUCESS = {"message": "Login Sucess!"}
-LOGOUT_SUCESS = {"message": "Logout Sucessfully!"}
-REGISTRATION_SUCESS = {"message": "Registered Sucessfully!"}
-BAD_CREDENTIALS = {"message": "Bad Credentials"}
-UNAUTHORIZED = {"message": "Unauthorized access"}
+LOGIN_SUCESS = {"success": True, "message": "Login Sucess!"}
+LOGOUT_SUCESS = {"success": True, "message": "Logout Sucessfully!"}
+REGISTRATION_SUCESS = {"success": True, "message": "Registered Sucessfully!"}
+BAD_CREDENTIALS = {"success": False, "message": "Bad Credentials"}
+UNAUTHORIZED = {"success": False, "message": "Unauthorized access"}
 
 # Event Organizer data
 
-NAME_EMPTY = {"message": "Name is required!"}
-ADDRESS_EMPTY = {"message": "Address is required!"}
-EMAIL_EMPTY = {"message": "Email is required!"}
-PASSWORD_EMPTY = {"message": "Password is required!"}
-STATUS_EMPTY = {"message": "Status is required!"}
-EMAIL_EXISTS = {"message": "Username already exists!"}
+NAME_EMPTY = {"success": False, "message": "Name is required!"}
+ADDRESS_EMPTY = {"success": False, "message": "Address is required!"}
+EMAIL_EMPTY = {"success": False, "message": "Email is required!"}
+PASSWORD_EMPTY = {"success": False, "message": "Password is required!"}
+STATUS_EMPTY = {"success": False, "message": "Status is required!"}
+EMAIL_EXISTS = {"success": False, "message": "Username already exists!"}
 
 # Events
-NO_EVENTS = {"message": "No Events at the moment"}
-EVENT_DOESNT_EXIST = {"message": "Event does not exist."}
-EVENT_NAME_EMPTY = {"message": "Event name is required!"}
-EVENT_DATE_EMPTY = {"message": "Event date is required!"}
-EVENT_EXISTS = {"message": "Event already existed!"}
-EVENT_SUCESS = {"message": "Event created sucessfully!"}
+NO_EVENTS = {"success": False, "message": "No Events at the moment"}
+EVENT_DOESNT_EXIST = {"success": False, "message": "Event does not exist."}
+EVENT_NAME_EMPTY = {"success": False, "message": "Event name is required!"}
+EVENT_DATE_EMPTY = {"success": False, "message": "Event date is required!"}
+EVENT_EXISTS = {"success": False, "message": "Event already existed!"}
+EVENT_SUCESS = {"success": True, "message": "Event created sucessfully!"}
+EVENT_RETRIEVED = {"success": True, "message": "Events retrieved successfully"}
 
 # Photographer
-PHOTOGRAPHER_EXISTS = {"message": "Photographer already registered!"}
+PHOTOGRAPHER_EXISTS = {"success": False, "message": "Photographer already registered!"}
 PHOTOGRAPHER_REGISTERED_SUCCESSFULLY = {
-    "message": "Photographer reigstered sucessfully!"
+    "success": True,
+    "message": "Photographer reigstered sucessfully!",
 }
 
 # Runner
-FIRST_NAME_EMPTY = {"message": "First name is required!"}
-LAST_NAME_EMPTY = {"message": "Last name is required!"}
-BIB_NO_EMPTY = {"message": "BIB Number is required!"}
-RUNNER_REGISTRATION_SUCCESSFUL = {"message": "Runner registered sucessfully!"}
-NO_RUNNERS = {"message": "No Runners registered at the moment"}
-NO_SINGLE_RUNNER = {"message": "Runner doesn't exists"}
+FIRST_NAME_EMPTY = {"success": False, "message": "First name is required!"}
+LAST_NAME_EMPTY = {"success": False, "message": "Last name is required!"}
+BIB_NO_EMPTY = {"success": False, "message": "BIB Number is required!"}
+RUNNER_REGISTRATION_SUCCESSFUL = {
+    "success": True,
+    "message": "Runner registered sucessfully!",
+}
+NO_RUNNERS = {"success": False, "message": "No Runners registered at the moment"}
+NO_SINGLE_RUNNER = {"success": False, "message": "Runner doesn't exists"}
+
+# Upload Image
+NO_IMAGES_INSERTED = {"message": "No inserted images"}
+IMAGE_SUCCESS = {"message": "Uploaded images successfully"}
